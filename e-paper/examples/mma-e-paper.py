c8ef8b27daf740375f2a58f3d833c06d278773ea
bfd <ben.dill@kroger.com>
2025-07-21 19:38:50 -0400

    chore(overlay): overlay-text is a named arg

--- e-paper/examples/mma-e-paper.py
+++ e-paper/examples/mma-e-paper.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+
 import sys
 import os
+import argparse
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
@@ -15,7 +17,12 @@ import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
-try:
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--overlay-text', type=str, default="", help="Text to overlay on the image")
+    args = parser.parse_args()
+    overlay_text = args.overlay_text
+
     logging.info("epd7in3f Demo")
 
     epd = epd7in3f.EPD()   
@@ -29,25 +36,18 @@ try:
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
     font40 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 40)
     
-    # # Drawing on the image
-    # logging.info("1.Drawing on the image...")
-    # Himage = Image.new('RGB', (epd.width, epd.height), epd.WHITE)  # 255: clear the frame
     draw = ImageDraw.Draw(Himage)
-    # draw.text((5, 0), 'hello world', font = font18, fill = epd.RED)
-    # draw.text((5, 20), '7.3inch e-Paper (F)', font = font24, fill = epd.YELLOW)
-    # read from an arg called overlay-text
-    overlay_text = sys.argv[1] if len(sys.argv) > 1 else ""
-
     draw.text((5, 45), overlay_text, font = font40, fill = epd.RED)
-    #draw.text((5, 45), 'days until worlds', font = font40, fill = epd.RED)
-    #draw.text((5, 85), '100', font = font40, fill = epd.RED)
 
-    epd.display(epd.getbuffer(Himage))    # font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    epd.display(epd.getbuffer(Himage))
 
-except IOError as e:
-    logging.info(e)
-    
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    epd7in3f.epdconfig.module_exit(cleanup=True)
-    exit()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except IOError as e:
+        logging.info(e)
+    except KeyboardInterrupt:
+        logging.info("ctrl + c:")
+        epd7in3f.epdconfig.module_exit(cleanup=True)
+        exit()
